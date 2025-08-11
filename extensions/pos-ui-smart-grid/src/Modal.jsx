79df191dd82b2a9c6301636d1d6b0ffacd5a5ce0
Kevin Vongmany <kevin.vongmany@gmail.com>
2025-08-11 13:38:33 +1000

    Rendered 2 buttons for app that applies 25% and $10 discounts

--- extensions/pos-ui-smart-grid/src/Modal.jsx
+++ extensions/pos-ui-smart-grid/src/Modal.jsx
@@ -4,17 +4,22 @@ import { Text, Screen, ScrollView, Navigator, useApi, reactExtension } from '@sh
 
 const Modal = () => {
   const api = useApi('pos.home.modal.render');
-  
+
   const onButtonPress = (type, title, amount) => {
     api.cart.applyCartDiscount(type, title, amount);
     api.toast.show("Discount applied successfully!");
   }
-  
+
   return (
     <Navigator>
       <Screen name="Discounts" title="Available Discounts!">
         <ScrollView>
-          <Text>Welcome to the extension!</Text>
+          <Button 
+            title="25%" 
+            onPress={() => onButtonPress("Percentage", "25% Off", "25")} />
+          <Button 
+            title="$10" 
+            onPress={() => onButtonPress("FixedAmount", "$10 Off", "10")} />
         </ScrollView>
       </Screen>
     </Navigator>
