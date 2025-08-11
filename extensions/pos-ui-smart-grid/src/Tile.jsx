2c3db4f702ea06cd5af94b18edbc39140661c817
Kevin Vongmany <kevin.vongmany@gmail.com>
2025-08-11 13:14:32 +1000

    update Tile component to enable state management and title

--- extensions/pos-ui-smart-grid/src/Tile.jsx
+++ extensions/pos-ui-smart-grid/src/Tile.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import { Tile, reactExtension, useApi } from '@shopify/ui-extensions-react/point-of-sale'
 
 const TileComponent = () => {
   const api = useApi()
+
+  const shouldEnable = (subtotal) => {
+    return Number(subtotal) > 100;
+  }
+
+  const [enabled, setEnabled] = useState(
+    shouldEnable(api.cart.subscribable.initial.subtotal)
+  );
+
   return (
     <Tile
-      title="My app"
+      title="Discount Example App"
       subtitle="SmartGrid react Extension"
       onPress={() => {
         api.action.presentModal()
